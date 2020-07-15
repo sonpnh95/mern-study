e4e555e7ae390ddf880c438cebc407debe7ff368
sonpnh95 <sonpnh95@gmail.com>
2020-07-15 15:38:13 +0700

    update normal config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import routeUrl from './common/routeUrl/routeUrl';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import FA from 'react-fontawesome'
+
+import Navbar from './layouts/Navbar'
+
+import ExercisesList from './components/exercises/ExercisesList'
+import ExercisesEdit from './components/exercises/ExercisesEdit'
+import ExercisesAdd from './components/exercises/ExercisesAdd'
+import UserAdd from './components/users/UserAdd'
 
 function App() {
   return (
-    <div className="contaner">
-      hello word
-    </div>
+    <Router>
+      <Navbar />
+      <FA name="accusoft" />
+      <Switch>
+        <Route path={routeUrl.exercisesList} component={ExercisesList} />
+        <Route path={routeUrl.exercisesEdit} component={ExercisesEdit} />
+        <Route path={routeUrl.exercisesAdd} component={ExercisesAdd} />
+        <Route path={routeUrl.userAdd} component={UserAdd} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/common/routeUrl/routeUrl.js
@@ -0,0 +1,9 @@
+const routeUrl = {
+  home: '/',
+  exercisesList: '/exercises',
+  exercisesEdit: '/exercises/edit/:id',
+  exercisesAdd: '/exercises/add',
+  userAdd: '/user/add',
+}
+
+export default routeUrl;
